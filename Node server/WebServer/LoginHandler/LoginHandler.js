8889655e06d47c6cfb36677dd70ebdaa08b05c9c
Shuezi <73438401+Shuezi@users.noreply.github.com>
2022-10-13 21:14:42 -0300

    cambios a /Register

--- Node server/WebServer/LoginHandler/LoginHandler.js
+++ Node server/WebServer/LoginHandler/LoginHandler.js
@@ -1,3 +1,4 @@
+
 class User{
     constructor(id){
         this.id = id;
--- Node server/index.js
+++ Node server/index.js
@@ -39,6 +39,23 @@ app.get('/', (req, res) => {
 
 })
 
+app.get('/Register', (req, res) => {
+  let ts = Date.now();
+  let date_ob = new Date(ts);
+  let date = date_ob.getDate();
+  let month = date_ob.getMonth() + 1;
+  let year = date_ob.getFullYear();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
+  let str = minutes+"-"+seconds ;
+  
+
+  console.log(` ${str} - Nueva request`);
+
+  res.sendFile(path.join(__dirname,'WebServer/Views/RegisterPage.html'));
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
