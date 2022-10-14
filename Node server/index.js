7d820dc079ecca84762118c286f53c72d2600359
Shuezi <73438401+Shuezi@users.noreply.github.com>
2022-10-13 21:09:49 -0300

    con redireccion

--- Node server/index.js
+++ Node server/index.js
@@ -2,12 +2,16 @@
 const express = require('express')
 const tessereact = require("tesseract.js");
 const fs = require("fs");
+const path = require("path");
+
 const app = express()
+app.use(express.static('WebServer/Views'))
 const port = 80
 
 app.get('/', (req, res) => {
     let ts = Date.now();
 
+    
     let date_ob = new Date(ts);
     let date = date_ob.getDate();
     let month = date_ob.getMonth() + 1;
@@ -15,18 +19,23 @@ app.get('/', (req, res) => {
     let minutes = date_ob.getMinutes();
     let seconds = date_ob.getSeconds();
     let str = minutes+"-"+seconds ;
+    
 
     console.log(` ${str} - Nueva request`);
+    /*
     var file = fs.readFile("./a.png",(err,data)=>{
         tessereact.recognize(data).then((textData)=>{
         console.log(` ${str} - ${textData.data.text}`);
 
     })
     
+    
 
 
     })
-    res.send('<H1>Hola Carlos!</H1>');
+    */
+
+    res.sendFile(path.join(__dirname,'WebServer/Views/LoginPage.html'));
 
 })
 
