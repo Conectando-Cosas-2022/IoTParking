32c58c988ec3a7bc23b6182b445ddd8c300c3a82
Shuezi <73438401+Shuezi@users.noreply.github.com>
2022-12-10 03:29:54 -0300

    Update Pruebas_Leds.ino

--- Pruebas_Leds/Pruebas_Leds.ino
+++ Pruebas_Leds/Pruebas_Leds.ino
@@ -1,4 +1,4 @@
-//Pin de datos
+//Pin de datos (pin 2 para tira1 y pin 4 para tira2)
 #define PIN 2
 #define pin2 4
 //Cantidad de pixeles de la led
@@ -18,6 +18,7 @@ uint32_t verde = tira1.Color(0, 255, 0);
 uint32_t otroColor = tira1.Color(75, 12, 110);
 //uint32_t negro = tira1.Color(0, 0, 0);
 
+//Definimos los largos que indican hasta que led debe encenderce para señalar el lugar asignado
 int largo1 = 6;
 int* lugar1 = new int[largo1];
 
@@ -31,9 +32,11 @@ int largo4 = 48;
 int* lugar4 = new int[largo4];
 
 void setup() {
-  // inicializamos tira1
+  // inicializamos tira1 y tira2
   tira1.begin();
   tira2.begin();
+
+  //Rellenamos los vectores de las led que deben ir encendidas
   for (int i = 0; i < largo1; i++) {
     lugar1[i] = i;
   }
@@ -67,12 +70,12 @@ void prender(int* lugar, int largo, int numTira, uint32_t color) {
 void loop() {
   int numTira = 1;
   prender(lugar3, largo3, numTira, rojo);
-  delay(3000);
+  delay(1000);
   prender(lugar4, largo4, numTira, verde);
-  delay(3000);
+  delay(1000);
   numTira = 2;
   prender(lugar1, largo1, numTira, otroColor);
-  delay(3000);
+  delay(1000);
   prender(lugar2, largo2, numTira, verde);
-  delay(3000);
+  delay(1000);
 }
